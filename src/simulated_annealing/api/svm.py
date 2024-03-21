7c17f7b0d13a11dc2ad0b262c1ba0fe505164608
Austin Hester <ahester57@gmail.com>
2024-03-20 23:58:21 -0500

    finish SVM
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- src/simulated_annealing/api/svm.py
+++ src/simulated_annealing/api/svm.py
@@ -19,6 +19,8 @@ except ModuleNotFoundError:
 
 from ..core.logger import logger
 
+TARGET_LABEL = 'Corona'
+
 
 def to_unix_epoch(dt):
     return dt.astype9
@@ -30,6 +32,15 @@ def normalize(df, column):
 
 
 def display_df(df:pd.DataFrame, show_missing:bool=True, show_info:bool=False) -> None:
+    """Display the DataFrame.
+    
+    :param df: The data.
+    :type df: pd.DataFrame
+    :param show_missing: Whether to show missing data counts.
+    :type show_missing: bool
+    :param show_info: Whether to show info on the data.
+    :type show_info: bool
+    """
     logger.info(f'DataFrame shape: {df.shape}')
     logger.info(f'First 5 rows:\n{df.head()}') # preview the first 5 rows
     if show_missing:
@@ -40,16 +51,44 @@ def display_df(df:pd.DataFrame, show_missing:bool=True, show_info:bool=False) ->
 
 
 def data_disposal(df:pd.DataFrame) -> pd.DataFrame:
+    """Clean-up the DataFrame.
+
+    Remove rows:
+        - missing any of the symptoms
+        - diagnosis of 'other'
+
+    :param df: The data.
+    :type df: pd.DataFrame
+    :return: The data without disposals.
+    :rtype: pd.DataFrame
+    """
     # remove rows missing too many values
-    required_fields=['Corona', 'Cough_symptoms', 'Fever', 'Sore_throat', 'Shortness_of_breath', 'Headache']
+    required_fields=[TARGET_LABEL, 'Cough_symptoms', 'Fever', 'Sore_throat', 'Shortness_of_breath', 'Headache']
     logger.info(f'Dropping the rows missing required columns: {required_fields}')
     df = df.dropna(subset=required_fields)
     # weird find, get rid of rows where diagnosis is 'other'
-    df = df[df['Corona'] != 'other']
+    df = df[df[TARGET_LABEL] != 'other']
     return df
 
 
 def data_conversions(df:pd.DataFrame) -> pd.DataFrame:
+    """Convert type in the DataFrame.
+
+    Update values:
+        - Convert bools to `0|1`.
+        - Fill nulls for typically-binary variables with 0.5.
+        - Scale 'Known_contact' with best-guess weights. Could use SA to fine-tune.
+        - convert 'Test_date' to unix epoch
+
+    :param df: The data.
+    :type df: pd.DataFrame
+    :return: The data without conversions.
+    :rtype: pd.DataFrame
+    """
+    # check for "weird" data
+    weird_check_cols = ['Test_date', TARGET_LABEL, 'Age_60_above', 'Sex', 'Known_contact']
+    for col in weird_check_cols:
+        logger.debug(f'Column "{col}" distinct values:\t{df[col].unique()}')
     # filter bools
     df = df.replace(to_replace={
         False: 0, True: 1,
@@ -57,10 +96,6 @@ def data_conversions(df:pd.DataFrame) -> pd.DataFrame:
         'No': 0, 'Yes': 1,
         'male': 0, 'female': 1
     })
-    # check for "weird" data
-    weird_check_cols = ['Test_date', 'Corona', 'Age_60_above', 'Sex', 'Known_contact']
-    for col in weird_check_cols:
-        logger.debug(f'Column "{col}" distinct values:\t{df[col].unique()}')
     # weird finds, fill null values for typically-binary variables with 0.5
     df = df.fillna({
         'Age_60_above': 0.5,
@@ -74,7 +109,7 @@ def data_conversions(df:pd.DataFrame) -> pd.DataFrame:
     return df.infer_objects()
 
 
-def main(dataset_filename:str, max_missing_vals:int=9) -> str:
+def main(dataset_filename:str) -> str:
     """ Execute the command.
     
     :param dataset_filename: Name of a file. Use .csv for now.
@@ -95,18 +130,53 @@ def main(dataset_filename:str, max_missing_vals:int=9) -> str:
     df = data_disposal(df)
     # Numerify some data
     df = data_conversions(df)
+    # shuffle rows
+    # when you shuffle rows, you are better prepared for future data scenarios
+    # but you lose model training-reproducibility
+    df = df.sample(frac=1)
     # display again
     display_df(df, show_info=True)
     # Split into Training/Validation sets
-    X = df.drop(['Corona'], axis=1)
-    y = df['Corona']
+    X = df.drop([TARGET_LABEL], axis=1)
+    y = df[TARGET_LABEL]
     # drop id. it would poison the data.
-    X.drop(['Ind_ID'], axis=1, inplace=True)
+    X = X.drop(['Ind_ID'], axis=1)
+    # after some thought, I will remove the 'Test_date' from training set.
+    X = X.drop(['Test_date'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-    logger.info(X_train.head())
-    for predictor in X_train.columns[1:]:
-        plt.hist(X_train[predictor])
-        plt.title(predictor)
-        plt.ylabel(predictor)
+    logger.info(f'Test set:\n{X_train.head()}')
+    # ndf = X_train.apply(lambda x: (x - x.mean()) / x.std(), axis=0)
+    # for predictor in ndf.columns[0:]:
+    #     plt.hist(ndf[predictor])
+    #     plt.title(predictor)
+    #     plt.ylabel(predictor)
+    #     plt.show()
+    # logger.info(f'Normalized:\n{ndf.head()}')
+    # Input features
+    logger.info(f'X_train: {X_train.shape}')
+    logger.info(f'X_test: {X_test.shape}')
+    if plt is not None:
+        sns.set()
+        plt.figure(figsize = (14,9))
+        #sns.heatmap(X_train.corr(), cmap='GnBu', annot=True)
+        plt.title('Correlation Graph')
+        # Plotting the heatmap to check the correlation between the Target Label and other features
+        sns.heatmap(df.corr()[[TARGET_LABEL]].sort_values(by=TARGET_LABEL, ascending=False), vmin=-1, vmax=1, annot=True, cmap='GnBu')
         plt.show()
+    # create and train the model
+    model = SVC(kernel='linear')
+    model.fit(X_train, y_train)
+    train_preds = model.predict(X_train)
+    test_preds = model.predict(X_test)
+    scores = {
+        'accuracy': {'train': accuracy_score(y_train, train_preds), 'test': accuracy_score(y_test, test_preds)},
+        'precision': {'train': precision_score(y_train, train_preds), 'test': precision_score(y_test, test_preds)},
+        'recall': {'train': recall_score(y_train, train_preds), 'test': recall_score(y_test, test_preds)},
+        'f1-score': {'train': f1_score(y_train, train_preds), 'test': f1_score(y_test, test_preds)},
+    }
+    logger.info(scores)
+    logger.info(y_test.head())
+    logger.info(test_preds[0:5])
+    scores_df = pd.DataFrame(scores)
+    logger.info(scores_df)
     return f'Hello, {dataset_filename}!'
